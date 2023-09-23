456027cd82b295d8f31606b3abe7d5628b27e723
prAhmed20 <ttbyqatandrwydmhkrh@gmail.com>
2023-09-24 01:15:52 +0300

    create header,sidebar,footer,order,products components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SideBarComponent } from './Components/side-bar/side-bar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { OrderComponent } from './Components/order/order.component';
+import { ProductsComponent } from './Components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideBarComponent,
+    FooterComponent,
+    OrderComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
